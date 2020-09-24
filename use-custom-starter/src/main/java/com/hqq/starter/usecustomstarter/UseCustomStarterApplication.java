77b520f1f01e2b6653adbb4613acb16eff8a2749
hqq518 <969375255@qq.com>
2020-09-25 00:41:15 +0800

    添加Eureka客户端配置

--- use-custom-starter/src/main/java/com/hqq/starter/usecustomstarter/UseCustomStarterApplication.java
+++ use-custom-starter/src/main/java/com/hqq/starter/usecustomstarter/UseCustomStarterApplication.java
@@ -2,8 +2,10 @@ package com.hqq.starter.usecustomstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  //激活Eureka客户端
 public class UseCustomStarterApplication {
 
     public static void main(String[] args) {
